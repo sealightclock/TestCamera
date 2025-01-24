36bc8079729807e4c851c7255621cc53840f91e9
Sealight Clock <chbebram@hotmail.com>
2025-01-24 15:44:46 -0800

    4th commit: MainActivity to display CameraScreen. Taking a picture does not work yet. Next task: request run-time permission.

--- app/src/main/java/com/example/jonathan/testcamera/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testcamera/MainActivity.kt
@@ -3,45 +3,17 @@ package com.example.jonathan.testcamera
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jonathan.testcamera.ui.theme.TestCameraTheme
+import com.example.jonathan.testcamera.presentation.view.CameraScreen
+import com.example.jonathan.testcamera.presentation.viewmodel.CameraViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            TestCameraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            CameraScreen(
+                viewModel = CameraViewModel()
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestCameraTheme {
-        Greeting("Android")
     }
 }
