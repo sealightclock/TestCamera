c61310b389a9e61a4c147ac6d7af993d15100b17
Sealight Clock <chbebram@hotmail.com>
2025-01-24 16:09:20 -0800

    5th commit: CameraScreen: Request run-time permission before taking a picture.

--- app/src/main/java/com/example/jonathan/testcamera/presentation/view/CameraScreen.kt
+++ app/src/main/java/com/example/jonathan/testcamera/presentation/view/CameraScreen.kt
@@ -1,5 +1,7 @@
 package com.example.jonathan.testcamera.presentation.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.ImageDecoder
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -7,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,12 +21,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.jonathan.testcamera.presentation.viewmodel.CameraViewModel
 
 @Composable
@@ -43,6 +50,23 @@ fun CameraScreen(viewModel: CameraViewModel) {
         }
     )
 
+    // Track permission state
+    var isCameraPermissionGranted by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    // Permission launcher
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        isCameraPermissionGranted = isGranted
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -61,11 +85,32 @@ fun CameraScreen(viewModel: CameraViewModel) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = { cameraLauncher.launch() },
-            modifier = Modifier.padding(8.dp)
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(text = "Take Picture", color = Color.White)
+            if (isCameraPermissionGranted) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Permission Granted. Ready to take a picture.")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        cameraLauncher.launch() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Take Picture", color = Color.White)
+                    }
+                }
+            } else {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Camera permission is required to take a picture.")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    }) {
+                        Text("Grant Permission")
+                    }
+                }
+            }
         }
     }
 }
